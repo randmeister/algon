5816cc0240f6739085a0fb436470bea5a263ca54
Patrick Wolleb <patrick.wolleb@srf.ch>
2021-11-27 00:17:04 +0100

    Add tests and statefulset

---
+++ test/connection.test.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const algosdk = require('algosdk')
+const util = require('util');
+const child_process = require('child_process')
+const exec = util.promisify(child_process.exec)
+const spawn = child_process.spawn
+const debug = require('debug')('algon:test')
+
+
+describe('Connection to internal service', () => {
+
+  let portForwardChildProcess
+
+  before(async () => {
+    await cmd('kubectl create namespace algon || true')
+    // await cmd('helm upgrade --install algon charts/algon --namespace algon')
+    // portForwardChildProcess = await cmd('kubectl port-forward service/algon 8080:8080', true)
+  }) 
+
+  after(async () => {
+    // await cmd(`kill -9 ${childProcess.pid}`)
+    // /await cmd('helm delete algon --namespace algon')
+  })
+
+  describe('with access token', () => {
+    it('should connect', async () => {
+      const token = 'Your algod API token'
+      const server = 'http://localhost'
+      const port = 8080
+      const client = new algosdk.Algodv2(token, server, port)
+      debug(1)
+      const res = await client.status().do()
+      debug(res.data)
+      
+
+    });
+  });
+});
+
+// Shell command with stdout/stderr logging
+async function cmd(cmdString, daemon=false) {
+  let childProcess 
+  if(daemon) {
+    childProcess = await spawn(cmdString, {
+      detached: true
+    })
+  } else {
+    childProcess = await exec(cmdString)
+  }
+  debug(`${cmdString} : stderr`, childProcess.stderr)
+  debug(`${cmdString} : stdout`, childProcess.stdout)
+  return childProcess
+}
